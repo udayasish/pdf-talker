c2c94033fe7eaec993da1fd38405eab8aa5faddb
Udayasish <udayasish@gmail.com>
2025-10-23 18:12:54 +0530

    https fix

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.233.198.145:8000/api/:path*", // Proxy to backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
